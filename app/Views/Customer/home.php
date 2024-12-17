e8622e0286c42dc5b15dc0b555bd1145263ab371
Literary_Oasis <lethanhba0505@gmail.com>
2024-12-17 18:54:12 +0700

    home degign css

--- app/Views/Customer/home.php
+++ app/Views/Customer/home.php
@@ -1,1 +1,143 @@
-<h1>Home</h1>
+<div class="banner">
+    <h1>Banner</h1>
+</div>
+
+<div class="row">
+    <div class="col-12">
+        <h1 class="text-center pt-3 pb-3">Danh mục</h1>
+    </div>
+
+    <div class="col-2">
+        <a href="" class=" catagory-item">
+            <div class="catagory-img">
+                <img src="<?= BASE_URL ?>/public/images/thoi-trang-nam.webp" alt="">
+            </div>
+            <h3 class="text-ellipsis" style="max-width: 150px;"> sdf à ádfasf á asdf as asdfasdDanh mục</h3>
+        </a>
+    </div>
+
+    <div class="col-2">
+        <a href="" class=" catagory-item">
+            <div class="catagory-img">
+                <img src="<?= BASE_URL ?>/public/images/thoi-trang-nu.webp" alt="">
+            </div>
+            <h3 class="text-ellipsis" style="max-width: 150px;"> sdf à ádfasf á asdf as asdfasdDanh mục</h3>
+        </a>
+    </div>
+
+    <div class="col-2">
+        <a href="" class=" catagory-item">
+            <div class="catagory-img">
+                <img src="<?= BASE_URL ?>/public/images/thoi-trang-tre-em.webp" alt="">
+            </div>
+            <h3 class="text-ellipsis" style="max-width: 150px;"> sdf à ádfasf á asdf as asdfasdDanh mục</h3>
+        </a>
+    </div>
+
+    <div class="col-2">
+        <a href="" class=" catagory-item">
+            <div class="catagory-img">
+                <img src="<?= BASE_URL ?>/public/images/nha-cua-doi-song.webp" alt="">
+            </div>
+            <h3 class="text-ellipsis" style="max-width: 150px;"> sdf à ádfasf á asdf as asdfasdDanh mục</h3>
+        </a>
+    </div>
+
+    <div class="col-2">
+        <a href="" class=" catagory-item">
+            <div class="catagory-img">
+                <img src="<?= BASE_URL ?>/public/images/dien-thoai-va-phu-kien.webp" alt="">
+            </div>
+            <h3 class="text-ellipsis" style="max-width: 150px;"> sdf à ádfasf á asdf as asdfasdDanh mục</h3>
+        </a>
+    </div>
+
+    <div class="col-2">
+        <a href="" class=" catagory-item">
+            <div class="catagory-img">
+                <img src="<?= BASE_URL ?>/public/images/dong-ho.webp" alt="">
+            </div>
+            <h3 class="text-ellipsis" style="max-width: 150px;"> sdf à ádfasf á asdf as asdfasdDanh mục</h3>
+        </a>
+    </div>
+
+    <div class="col-2">
+        <a href="" class=" catagory-item">
+            <div class="catagory-img">
+                <img src="<?= BASE_URL ?>/public/images/may-tinh.webp" alt="">
+            </div>
+            <h3 class="text-ellipsis" style="max-width: 150px;"> sdf à ádfasf á asdf as asdfasdDanh mục</h3>
+        </a>
+    </div>
+
+    <div class="col-2">
+        <a href="" class=" catagory-item">
+            <div class="catagory-img">
+                <img src="<?= BASE_URL ?>/public/images/nha-sach-online.webp" alt="">
+            </div>
+            <h3 class="text-ellipsis" style="max-width: 150px;"> sdf à ádfasf á asdf as asdfasdDanh mục</h3>
+        </a>
+    </div>
+
+    <div class="col-2">
+        <a href="" class=" catagory-item">
+            <div class="catagory-img">
+                <img src="<?= BASE_URL ?>/public/images/phu-kien-trang-suc.webp" alt="">
+            </div>
+            <h3 class="text-ellipsis" style="max-width: 150px;"> sdf à ádfasf á asdf as asdfasdDanh mục</h3>
+        </a>
+    </div>
+
+    <div class="col-2">
+        <a href="" class=" catagory-item">
+            <div class="catagory-img">
+                <img src="<?= BASE_URL ?>/public/images/the-thao-va-du-lich.webp" alt="">
+            </div>
+            <h3 class="text-ellipsis" style="max-width: 150px;"> sdf à ádfasf á asdf as asdfasdDanh mục</h3>
+        </a>
+    </div>
+
+    <div class="col-2">
+        <a href="" class=" catagory-item">
+            <div class="catagory-img">
+                <img src="<?= BASE_URL ?>/public/images/thiet-bi-dien-tu.webp" alt="">
+            </div>
+            <h3 class="text-ellipsis" style="max-width: 150px;"> sdf à ádfasf á asdf as asdfasdDanh mục</h3>
+        </a>
+    </div>
+
+    <div class="col-2">
+        <a href="" class=" catagory-item">
+            <div class="catagory-img">
+                <img src="<?= BASE_URL ?>/public/images/thiet-bi-gia-dung.webp" alt="">
+            </div>
+            <h3 class="text-ellipsis" style="max-width: 150px;"> sdf à ádfasf á asdf as asdfasdDanh mục</h3>
+        </a>
+    </div>
+
+</div>
+
+<div class="row">
+    <div class="col-12">
+        <h1 class="text-center pt-3 pb-3 mt-5 bg-3 text-color-1">Gợi ý cho bạn</h1>
+    </div>
+
+    <div class="col-2">
+        <a href="" class="product-item p-2">
+            <img src="<?= BASE_URL ?>/public/uploads/img/cap-sac.webp" alt="">
+            <span class="text-ellipsis-2 mt-2 fw-bold fs-4">20w Sạc Nhanh Thông Minh USB C PD US Cắm Dữ Liệu Sạc Nhanh 1Meter PD Cáp Adapter USB-C Dây Cắm</span>
+            <div class="">
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-regular fa-star"></i>
+                <i class="fa-regular fa-star"></i>
+            </div>
+            <h4><del>3.000.000đ</del></h4>
+            <div class="d-flex justify-content-between">
+                <h3>200.000đ</h3>
+                <span class="fs-6">Đã bán 10k</span>
+            </div>
+        </a>
+    </div>
+</div>
