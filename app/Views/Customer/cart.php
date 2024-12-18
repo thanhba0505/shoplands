2d603c18b70ec32cbe92e1f2f2ffaa00a25af91d
Literary_Oasis <lethanhba0505@gmail.com>
2024-12-18 13:10:46 +0700

    css cart continue

--- app/Views/Customer/cart.php
+++ app/Views/Customer/cart.php
@@ -1,19 +1,273 @@
 <div class="row">
     <div class="col-12">
-        <table class="table table-borderless table-light">
-            <tr>
-                <td>
-                    <div class="checkbox ml-4 mt-2">
-                        <input type="checkbox" id="custom-checkbox" class="custom-checkbox">
-                        <label for="custom-checkbox" class="checkbox-label"></label>
-                    </div>
+        <table class="table table-borderless table-light table-align-middle table-cart">
 
-                    <a class="mb-3" href="<?= BASE_URL ?>/shop" style="margin-left: 60px; padding-bottom: 2px;">Tên shop</a>
-               <hr> </td>
+            <!-- Item 1 -->
+            <tr class="table-border-bottom">
+                <td width="100px">
+                    <div class="d-flex" style="margin-left: 36px;">
+                        <div class="checkbox">
+                            <input type="checkbox" id="custom-checkbox" class="custom-checkbox">
+                            <label for="custom-checkbox" class="checkbox-label"></label>
+                        </div>
+                    </div>
+                </td>
+                <td colspan="2">
+                    <div class="d-flex"><a href="<?= BASE_URL ?>/shop">Tên shop</a></div>
+                </td>
+                <td class="text-center">
+                    <span class="fs-5">Phân loại</span>
+                </td>
+                <td class="text-center">
+                    <span class="fs-5">Đơn giá</span>
+                </td>
+                <td class="text-center">
+                    <span class="fs-5">Số lượng</span>
+                </td>
+                <td class="text-center">
+                    <span class="fs-5">Tổng tiền</span>
+                </td>
+                <td class="text-center">
+                    <span class="fs-5">Hành động</span>
+                </td>
             </tr>
 
             <tr>
-                <td></td>
+                <td width="100px">
+                    <div class="d-flex" style="margin-left: 36px;">
+                        <div class="checkbox">
+                            <input type="checkbox" id="custom-checkbox" class="custom-checkbox">
+                            <label for="custom-checkbox" class="checkbox-label"></label>
+                        </div>
+                    </div>
+                </td>
+
+                <td width="100px">
+                    <div class="cart-img"><img src="<?= BASE_URL ?>/public/uploads/img/cap-sac.webp" alt=""></div>
+                </td>
+
+                <td width="">
+                    <span class="text-ellipsis-4 fw-bold fs-5 text-dark-color">20w Sạc Nhanh Thông Minh USB C PD US Cắm Dữ Liệu Sạc Nhanh 1Meter PD Cáp Adapter USB-C Dây Cắm</span>
+                </td>
+
+                <td width="150px" class="text-center">
+                    <span class="fs-5">Bộ sạc và cáp<br>20W<br>USB-C</span>
+                </td>
+
+                <td width="150px" class="text-center">
+                    <h6><del>3.000.000đ</del></h6>
+                    <h5>200.000đ</h5>
+                </td>
+
+                <td width="150px" class="text-center">
+                    <input type="number" id="username" class="form-control" placeholder="Số lượng">
+                </td>
+
+                <td width="150px" class="text-center">
+                    <h4 class="text-color-3">200.000đ</h4>
+                </td>
+
+                <td width="150px" class="text-center">
+                    <a href="" class="btn-outline">Xóa</a>
+                </td>
+
+            </tr>
+
+            <tr>
+                <td width="100px">
+                    <div class="d-flex" style="margin-left: 36px;">
+                        <div class="checkbox">
+                            <input type="checkbox" id="custom-checkbox" class="custom-checkbox">
+                            <label for="custom-checkbox" class="checkbox-label"></label>
+                        </div>
+                    </div>
+                </td>
+
+                <td width="100px">
+                    <div class="cart-img"><img src="<?= BASE_URL ?>/public/uploads/img/cap-sac.webp" alt=""></div>
+                </td>
+
+                <td width="">
+                    <span class="text-ellipsis-4 fw-bold fs-5 text-dark-color">20w Sạc Nhanh Thông Minh USB C PD US Cắm Dữ Liệu Sạc Nhanh 1Meter PD Cáp Adapter USB-C Dây Cắm</span>
+                </td>
+
+                <td width="150px" class="text-center">
+                    <span class="fs-5">Bộ sạc và cáp<br>20W<br>USB-C</span>
+                </td>
+
+                <td width="150px" class="text-center">
+                    <h6><del>3.000.000đ</del></h6>
+                    <h5>200.000đ</h5>
+                </td>
+
+                <td width="150px" class="text-center">
+                    <input type="number" id="username" class="form-control" placeholder="Số lượng">
+                </td>
+
+                <td width="150px" class="text-center">
+                    <h4 class="text-color-3">200.000đ</h4>
+                </td>
+
+                <td width="150px" class="text-center">
+                    <a href="" class="btn-outline">Xóa</a>
+                </td>
+
+            </tr>
+
+            <tr class="table-border-top">
+                <td>
+                    <div class="text-color-3 fs-3" style="margin-left: 36px;">
+                        <i class="fa-solid fa-ticket"></i>
+                    </div>
+                </td>
+
+                <td colspan="7">Xem tất cả voucher của shop</td>
+            </tr>
+
+            <tr class="table-border-top">
+                <td>
+                    <div class="text-color-3 fs-3" style="margin-left: 36px;">
+                        <i class="fa-solid fa-truck-fast"></i>
+                    </div>
+                </td>
+
+                <td colspan="7">Giảm ₫300.000 phí vận chuyển đơn tối thiểu ₫0; Giảm ₫500.000 phí vận chuyển đơn tối thiểu ₫500.000</td>
+            </tr>
+
+            <!-- Item 2 -->
+            <tr class="table-color-1">
+                <td colspan="8"></td>
+            </tr>
+
+            <tr class="table-border-bottom">
+                <td width="100px">
+                    <div class="d-flex" style="margin-left: 36px;">
+                        <div class="checkbox">
+                            <input type="checkbox" id="custom-checkbox" class="custom-checkbox">
+                            <label for="custom-checkbox" class="checkbox-label"></label>
+                        </div>
+                    </div>
+                </td>
+                <td colspan="2">
+                    <div class="d-flex"><a href="<?= BASE_URL ?>/shop">Tên shop</a></div>
+                </td>
+                <td class="text-center">
+                    <span class="fs-5">Phân loại</span>
+                </td>
+                <td class="text-center">
+                    <span class="fs-5">Đơn giá</span>
+                </td>
+                <td class="text-center">
+                    <span class="fs-5">Số lượng</span>
+                </td>
+                <td class="text-center">
+                    <span class="fs-5">Tổng tiền</span>
+                </td>
+                <td class="text-center">
+                    <span class="fs-5">Hành động</span>
+                </td>
+            </tr>
+
+            <tr>
+                <td width="100px">
+                    <div class="d-flex" style="margin-left: 36px;">
+                        <div class="checkbox">
+                            <input type="checkbox" id="custom-checkbox" class="custom-checkbox">
+                            <label for="custom-checkbox" class="checkbox-label"></label>
+                        </div>
+                    </div>
+                </td>
+
+                <td width="100px">
+                    <div class="cart-img"><img src="<?= BASE_URL ?>/public/uploads/img/cap-sac.webp" alt=""></div>
+                </td>
+
+                <td width="">
+                    <span class="text-ellipsis-4 fw-bold fs-5 text-dark-color">20w Sạc Nhanh Thông Minh USB C PD US Cắm Dữ Liệu Sạc Nhanh 1Meter PD Cáp Adapter USB-C Dây Cắm</span>
+                </td>
+
+                <td width="150px" class="text-center">
+                    <span class="fs-5">Bộ sạc và cáp<br>20W<br>USB-C</span>
+                </td>
+
+                <td width="150px" class="text-center">
+                    <h6><del>3.000.000đ</del></h6>
+                    <h5>200.000đ</h5>
+                </td>
+
+                <td width="150px" class="text-center">
+                    <input type="number" id="username" class="form-control" placeholder="Số lượng">
+                </td>
+
+                <td width="150px" class="text-center">
+                    <h4 class="text-color-3">200.000đ</h4>
+                </td>
+
+                <td width="150px" class="text-center">
+                    <a href="" class="btn-outline">Xóa</a>
+                </td>
+
+            </tr>
+
+            <tr>
+                <td width="100px">
+                    <div class="d-flex" style="margin-left: 36px;">
+                        <div class="checkbox">
+                            <input type="checkbox" id="custom-checkbox" class="custom-checkbox">
+                            <label for="custom-checkbox" class="checkbox-label"></label>
+                        </div>
+                    </div>
+                </td>
+
+                <td width="100px">
+                    <div class="cart-img"><img src="<?= BASE_URL ?>/public/uploads/img/cap-sac.webp" alt=""></div>
+                </td>
+
+                <td width="">
+                    <span class="text-ellipsis-4 fw-bold fs-5 text-dark-color">20w Sạc Nhanh Thông Minh USB C PD US Cắm Dữ Liệu Sạc Nhanh 1Meter PD Cáp Adapter USB-C Dây Cắm</span>
+                </td>
+
+                <td width="150px" class="text-center">
+                    <span class="fs-5">Bộ sạc và cáp<br>20W<br>USB-C</span>
+                </td>
+
+                <td width="150px" class="text-center">
+                    <h6><del>3.000.000đ</del></h6>
+                    <h5>200.000đ</h5>
+                </td>
+
+                <td width="150px" class="text-center">
+                    <input type="number" id="username" class="form-control" placeholder="Số lượng">
+                </td>
+
+                <td width="150px" class="text-center">
+                    <h4 class="text-color-3">200.000đ</h4>
+                </td>
+
+                <td width="150px" class="text-center">
+                    <a href="" class="btn-outline">Xóa</a>
+                </td>
+
+            </tr>
+
+            <tr class="table-border-top">
+                <td>
+                    <div class="text-color-3 fs-3" style="margin-left: 36px;">
+                        <i class="fa-solid fa-ticket"></i>
+                    </div>
+                </td>
+
+                <td colspan="7">Xem tất cả voucher của shop</td>
+            </tr>
+
+            <tr class="table-border-top">
+                <td>
+                    <div class="text-color-3 fs-3" style="margin-left: 36px;">
+                        <i class="fa-solid fa-truck-fast"></i>
+                    </div>
+                </td>
+
+                <td colspan="7">Giảm ₫300.000 phí vận chuyển đơn tối thiểu ₫0; Giảm ₫500.000 phí vận chuyển đơn tối thiểu ₫500.000</td>
             </tr>
         </table>
     </div>
