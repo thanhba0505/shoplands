bc875e91fe74d1428a1c11758fb0a0cc3ad989ad
Literary_Oasis <lethanhba0505@gmail.com>
2025-01-04 15:12:16 +0700

    fix pw

--- _db/database/seeders/UserSeeder.php
+++ _db/database/seeders/UserSeeder.php
@@ -18,7 +18,7 @@ class UserSeeder extends Seeder
         User::factory()->create([
             'name' => 'test user 2',
             'phone' => '456',
-            'password' => password_hash('123', PASSWORD_ARGON2I),
+            'password' => password_hash('456', PASSWORD_ARGON2I),
         ]);
 
         User::factory()->count(20)->create(); // Tạo 20 người dùng
