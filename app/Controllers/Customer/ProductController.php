3212e7438222084678b6b54bb14471f4c7f1eb9a
PhamNgocTuan <phamngoctuan191003@gmail.com>
2025-01-13 18:31:23 +0700

    message

--- app/Controllers/Customer/ProductController.php
+++ app/Controllers/Customer/ProductController.php
@@ -1,12 +1,18 @@
 <?php
+require_once 'app/Models/Product.php';
 
 class ProductController
 {
     public function show()
     {
+        // Gọi model đến sản phẩm
+        $product = new Product();
+        $product_result = $product->getProducts();
+
         $data = [
             'title' => 'Product Page',
-            'sidebar' => 'products'
+            'sidebar' => 'products',
+            'products' => $product_result ? $product_result : []
         ];
 
         View::make('Customer/products', $data, 'layout/layout-sidebar');
