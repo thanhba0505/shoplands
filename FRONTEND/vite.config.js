50196d9de8ba8d682df1e5ceb273a4a917f9e356
Literary_Oasis <lethanhba0505@gmail.com>
2025-04-28 15:27:32 +0700

    config vite

--- FRONTEND/vite.config.js
+++ FRONTEND/vite.config.js
@@ -3,11 +3,14 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: [{ find: "~", replacement: "/src" }],
-    },
-    define: {
-        "process.env": {},
-    },
+  plugins: [react()],
+  resolve: {
+    alias: [{ find: "~", replacement: "/src" }],
+  },
+  define: {
+    "process.env": {},
+  },
+  server: {
+    historyApiFallback: true,
+  },
 });
